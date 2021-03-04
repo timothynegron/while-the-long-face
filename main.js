f5367c76a60fc42873bfd5d314ef24bb31787318
Timothy Negron <timothy.negron@codeimmersives.com>
2021-03-04 14:22:58 -0500

    Adds functions. All test passed.

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 function numberDoubler(num) {
     // Your code here
+    while(num < 100){
+        num *= 2;
+    }
+    return num;
 }
 
 function stringRepeater(str) {
     // Your code here
+    let originalString = str;
+    while(str.length < 10){
+        str += originalString;
+    }
+    return str;
 }
 
 function makeDivisible(x, y) {
     // Your code here
+    while( x % y !== 0){
+        x++;
+    }
+    return x;
 }
 
 module.exports = {
